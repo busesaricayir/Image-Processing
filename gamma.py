03381c58a04079b6c15f2c8c4c2966cefeb5f4c6
Buse SARIÇAYIR <107461852+busesaricayir@users.noreply.github.com>
2022-11-14 13:15:44 +0300

    Update gamma.py

--- gamma.py
+++ gamma.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 img = cv2.imread('img/img3.png')
 i = 1
-for gamma in [0.1, 0.5, 1.2, 2.2]:
+for gamma in [3.0, 4.0, 5.0]:
     gamma_corrected = np.array(255 * (img / 255) ** gamma, dtype='uint8')
     plt.subplot(2, 2, i), plt.imshow(gamma_corrected, cmap='gray')
     plt.title(str(gamma))
